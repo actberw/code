844c268299ed68f5113b736e2ce7753d5257d7b3
actberw <actberw@gmail.com>
2013-12-16 17:56:41 +0800

    set alias scope

--- shell/git-alias.sh
+++ shell/git-alias.sh
@@ -3,19 +3,19 @@
 # config user name and email
 echo -n "User name is:"
 read USERNAME
-git config --local user.name $USERNAME
+git config  user.name $USERNAME
 echo -n "User email is:"
 read EMAIL
-git config --local user.email $EMAIL
+git config user.email $EMAIL
 
 
 #config alias
 
-git config --local color.ui true
-git config --local alias.ci commit
-git config --local alias.st status
-git config --local alias.co checkout
-git config --local core.editor vi 
+git config --global color.ui true
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global core.editor vi 
 
 # show all config
 git config --list
