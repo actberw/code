417e700e65e90504094eb8b1103f1cd2447f2353
actberw <actberw@gmail.com>
2013-12-16 17:02:24 +0800

    add thrift install guid for centos

--- shell/install-scribe.sh
+++ shell/install-scribe.sh
@@ -1,8 +1,14 @@
 #/usr/bin/env bash
 cd
 echo "Install dependencies"
-aptitude install autoconf automake libtool pkg-config libtool libevent-dev libevent-2.0-5 libboost-all-dev
-echo "Download and extract source file"
+aptitude install autoconf automake pkg-config libtool libevent-dev libevent-2.0-5 libboost-all-dev
+# centos 
+# sudo yum install libevent libevent-devel openssl-devel
+# wget http://sourceforge.net/projects/boost/files/boost/1.46.0/boost_1_46_0.tar.gz/download;tar zxvf boost_1_46_0.tar.gz;cd boost_1_46_0;
+# ./bootstrap.sh --prefix=/usr/local
+# sudo ./bjam --with-filesystem --with-system install # bjam --show-libraries
+# ldconfig
+echo "Download thrift and extract source file"
 wget http://archive.apache.org/dist/thrift/0.9.0/thrift-0.9.0.tar.gz
 tar zxvf thrift-0.9.0.tar.gz
 cd thrift-0.9.0
@@ -24,7 +30,8 @@ make install
 
 cd
 echo "Install scribe"
-git clone https://github.com/facebook/scribe.git
+#git clone https://github.com/facebook/scribe.git
+wget https://github.com/facebook/scribe/archive/master.zip;unzip master.zip
 cd scribe
 ./bootstrap.sh
 ./configure CPPFLAGS="-DHAVE_INTTYPES_H -DHAVE_NETINET_IN_H -DBOOST_FILESYSTEM_VERSION=2"
